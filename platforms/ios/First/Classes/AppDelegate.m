c8be773e2f50a21f2627a7d7435adee0e68c2487
Jacques Pupponi <jacques.pupponi@vnext.fr>
2017-02-03 14:45:14 +0100

    refactoring appgroup

--- platforms/ios/First/Classes/AppDelegate.m
+++ platforms/ios/First/Classes/AppDelegate.m
@@ -161,19 +161,25 @@
     
 }
 
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+    // pour Traitement AppGroup
+    [[POMAPI sharedInstance] WillTerminate];
+    
+}
+
+
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
+    // Pour Background Timeout
     [[POMAPI sharedInstance] DidEnterBackground];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     
-    [[POMAuthentificationService sharedManager] checkSecurity];
-    [[POMService sharedManager]refreshMenuPom];
-    
+    // pour Traitement AppGroup
     [[POMAPI sharedInstance] DidEnterForeground];
 }
 
